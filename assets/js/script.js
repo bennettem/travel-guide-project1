5dbbe2ef505dca8d5356f1c07a275f03cd80d1cd
0ffpudding <jennaanderson00@gmail.com>
2022-04-12 20:55:14 -0500

    created functions to pass data into separate cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(function () {
     var weatherApiKey = 'e0a6afee4ced0e5525c4a7c68c4ed596';
-    var city = 'Milwaukee';
+    // var city = 'Milwaukee';
     var form1 = $("#form1");
     var form2 = $("#form2");
 
@@ -8,11 +8,12 @@ $(function () {
         event.preventDefault();
         console.log(event.target.id);
         var city = $(event.target).find("input").val();
-        getLocationData(city, event.target.id)
+        getLocationData(city, event.target.id);
     }
     
     // Use the input city and fetch data from openweathermap
     function getLocationData(city, formId) {
+        $("#" + formId).siblings(".addData").empty();
         fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + weatherApiKey)
         .then(function(response) {
             return response.json()
@@ -36,14 +37,14 @@ $(function () {
             for (i = 0; i < 6; i++) {
                 // Create a card for each day
                 var dayCard = $('<div>')
-                    .addClass();
+                    .addClass("card");
                 // Set a header to the card with the date
-                var dayCardHeader = $('<h5>')
-                    .addClass()
-                    .text(moment().add(i, 'd').format('MM/DD/YYYY'));
+                var dayCardHeader = $('<div>')
+                    .addClass("card-divider")
+                    .html(moment().add(i, 'd').format('MM/DD/YYYY'));
                 // Create a div for the body of the card
                 var dayCardBody = $('<div>')
-                    .addClass();
+                    .addClass("card-section");
                 // Create an icon for the weather
                 var dayWeatherIcon = $('<img>')
                     .attr('src', 'http://openweathermap.org/img/wn/' + futureData.daily[i].weather[0].icon + '.png');
@@ -62,12 +63,12 @@ $(function () {
                 // Add the card header and card body to the card
                 dayCard.append(dayCardHeader, dayCardBody);
                 // Add the card to the forecast row
-                // $('#forecast').append(dayCard);
+                
+                $("#" + formId).siblings(".addData").append(dayCard);
             }
         })
     }
-
-    getLocationData(city); 
+    // getLocationData(city); 
     form1.on("submit", handleFormSubmit);
 
     form2.on("submit", handleFormSubmit);
